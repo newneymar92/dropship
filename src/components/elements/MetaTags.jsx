a51556df2b8c8c60403ef856c058c4f869605895
newneymar92 <nguyenanhtai.hust@gmail.com>
2023-11-23 16:48:01 +0700

    update thumb

--- src/components/elements/MetaTags.jsx
+++ src/components/elements/MetaTags.jsx
@@ -1,17 +1,26 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-const MetaTags = ({title}) => {
-    return ( 
-        <Head>
-            {/* Basic metas */}
-            <meta charSet="utf-8" />
-            <meta name="robots" content="noindex, follow" />
-            <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-            <meta name="description" content="eTrade eCommerce React Next JS Template" />
-            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-            <title>{`${title?title:"eTrade"} - eCommerce React Next JS Template`}</title>
-        </Head>
-     );
-}
- 
+const MetaTags = ({ title }) => {
+  return (
+    <Head>
+      {/* Basic metas */}
+      <meta charSet="utf-8" />
+      <meta name="robots" content="noindex, follow" />
+      <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+      <meta name="description" content="lngoude" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+      />
+      <meta
+        property="og:image"
+        content="https://lngoude.com/images/bg/bg-image-12.jpg"
+      />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <title>lngoude</title>
+    </Head>
+  );
+};
+
 export default MetaTags;
